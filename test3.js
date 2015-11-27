e7aff463f4152eb3dc26eca5ec554eab09925fce
clement <maboiteaspam@wanadoo.fr>
2015-11-28 01:44:51 +0800

    init

---
+++ test3.js
@@ -0,0 +1,51 @@
+
+var minimist = require('minimist')(process.argv.slice(2));
+
+if (minimist.verbose || minimist.v)
+  process.env['DEBUG'] = 'stream-rate-limiter';
+
+var debug = require('debug')('stream-rate-limiter')
+var through2 = require('through2')
+var rateLimiter = require('./index')
+var sVisualizer = require('./stream-visualizer')
+
+var concurrency=1000;
+var streamSize=5000;
+
+var visualizer = new sVisualizer();
+visualizer.start(streamSize);
+
+var streamC = through2.obj();
+var streamB = through2.obj();
+streamB.resume();
+
+var stream = rateLimiter(concurrency, function damnSlowProcess (chunk, done) {
+  setTimeout(function(){
+    //console.log(chunk.some+'');
+    done();
+  }, 250)
+});
+stream.resume();
+
+streamC
+.pipe(visualizer.jobSent('streamC'))
+.pipe(visualizer.jobRemains('streamC', true))
+
+streamC
+  .pipe(visualizer.jobSent('stream'))
+  .pipe(visualizer.jobProcessing('stream'))
+  .pipe(stream)
+  .pipe(visualizer.jobRemains('stream', true))
+
+
+stream
+  .pipe(visualizer.jobSent('streamB'))
+  .pipe(streamB)
+  .pipe(visualizer.jobRemains('streamB', true))
+
+for (var e=0; e<streamSize;e++) {
+  streamC.write({some:' data '+e})
+}
+streamC.end()
+
+
